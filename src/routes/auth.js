cc1b65e4a303b375f3600e8cb34cda99cc096a95
amanxsays <amanjhasahab07@gmail.com>
2026-01-21 18:27:35 +0530

    otp in console

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -49,9 +49,8 @@ authRouter.post("/otp", async (req,res)=>{
         if(otpALreadyThere) res.status(402).json({message: "OTP is ALready sent", data: otpALreadyThere.createdAt});
         const otpObj= new Otp({emailId: emailId, otp: Math.floor(100000 + Math.random() * 900000)});
         await otpObj.save();
-        console.log(otpObj.otp);
         sendEmail(emailId,"Otp Verification of your email on DevTinder","",otpTemplate(otpObj.otp));
-        res.json({message:"Otp sent to your email", data:otpObj.createdAt});
+        res.json({message:`Otp sent to your email :${otpObj.otp}`, data:otpObj.createdAt});
     } catch (error) {
         res.status(400).send("Error: " +error.message || error.data.message)
     }
