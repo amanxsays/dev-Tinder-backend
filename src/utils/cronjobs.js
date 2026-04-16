ae7c1caa62e260ace171eb22cc83f7006bfff3b8
amanxsays <amanjhasahab07@gmail.com>
2026-04-16 13:45:42 +0530

    fixxed the mail otp error

--- src/utils/cronjobs.js
+++ src/utils/cronjobs.js
@@ -4,12 +4,14 @@ const ConnectionRequest = require('../models/connectionRequests');
 const { sendEmail }= require("./send-email");
 const notificationTemplate = require('./templates/notificationTemplate');
 
-cron.schedule('0 10 * * *',async () => {
-    //10 am everyday
-  const toEmails=await findUsersHavingRequests();
-  for(const to of toEmails){
-    const res=await sendEmail(to,"Hiii User 👋🏻","",notificationTemplate());
-}});
+if (process.env.RUN_EMAIL_CRON === "true") {
+    cron.schedule('0 10 * * *',async () => {
+        //10 am everyday
+    const toEmails=await findUsersHavingRequests();
+    for(const to of toEmails){
+        const res=await sendEmail(to,"Hiii User 👋🏻","",notificationTemplate());
+    }})
+};
 
 //data to whom we have to send mails: i.e those connection requests which are tagged as interested  and was of yesterday we need toUserId of that and then corres. email
 const findUsersHavingRequests=async ()=>{
--- src/utils/send-email.js
+++ src/utils/send-email.js
@@ -1,30 +1,64 @@
+// require("dotenv").config();
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   //host: "smtp.gmail.com",
+//   service:"gmail",
+//   //port: 465,
+//   //secure: true,
+//   auth: {
+//     user: "amanjhasahab07@gmail.com",
+//     pass: process.env.EMAIL_KEY
+//   },
+// });
+
+// const sendEmail=async (to,subject,text,html) => {
+//     try {
+//         const info = await transporter.sendMail({
+//             from: '"DevTinder" <amanjhasahab07@gmail.com>',
+//             to,
+//             subject,
+//             text,
+//             html
+//     });
+//     console.log("Mail sent:", info.response);
+//     } catch (error) {
+//         console.error("Error sending email:", error.message);
+//         throw error;
+//     }
+// };
+// module.exports ={sendEmail};
+
+
 require("dotenv").config();
-const nodemailer = require("nodemailer");
+const axios = require('axios');
+const https = require('https');
+const httpsAgent = new https.Agent({ family: 4 });
 
-const transporter = nodemailer.createTransport({
-  //host: "smtp.gmail.com",
-  service:"gmail",
-  //port: 465,
-  //secure: true,
-  auth: {
-    user: "amanjhasahab07@gmail.com",
-    pass: process.env.EMAIL_KEY
-  },
-});
-
-const sendEmail=async (to,subject,text,html) => {
+const sendEmail = async (to, subject, text, html) => {
     try {
-        const info = await transporter.sendMail({
-            from: '"DevTinder" <amanjhasahab07@gmail.com>',
-            to,
-            subject,
-            text,
-            html
-    });
-    console.log("Mail sent:", info.response);
+        const response = await axios.post(
+            'https://mailserver.automationlounge.com/api/v1/messages/send',
+            {
+                to: to,
+                subject: subject,
+                html: html 
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${process.env.PROMAIL_API_KEY}`,
+                    'Content-Type': 'application/json',
+                },
+                httpsAgent: httpsAgent
+            }
+        );
+
+        console.log("Mail sent via Promailer API:", response.data);
+        return response.data;
     } catch (error) {
-        console.error("Error sending email:", error.message);
+        console.error("Error sending API email:", error.response?.data || error.message);
         throw error;
     }
 };
-module.exports ={sendEmail};
+
+module.exports = { sendEmail };
